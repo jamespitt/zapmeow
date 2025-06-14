d44d67c53300ddc4ab5f4884ad9dde3140c1d24c
James Pitt <james.pitt@gmail.com>
2025-06-14 13:18:49 +0000

    updates

--- api/repository/transcription_repository.go
+++ api/repository/transcription_repository.go
@@ -36,14 +36,3 @@ func (r *transcriptionRepository) FindByMessageID(messageID string) (*model.Tran
 	return &transcription, nil
 }
 
-func (r *transcriptionRepository) FindByMessageID(messageID string) (*model.Transcription, error) {
-	var transcription model.Transcription
-	result := r.db.Client().Where("message_id = ?", messageID).First(&transcription)
-	if result.Error != nil {
-		if result.Error.Error() == "record not found" {
-			return nil, nil // Return nil transcription and nil error if not found
-		}
-		return nil, result.Error // Return error if something else went wrong
-	}
-	return &transcription, nil
-}
